be857fd8996a329bf7cf2d1bc961919ca44314a3
mininja610 <yuukingu610@gmail.com>
2022-12-18 06:54:10 +0900

    add1218

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <body>
+        <header>
+            <!--<img class=logo url=>-->
+            <h1>Blog name</h1>
+            
+            <div class=topic>
+                
+            </div>
+        </header>
+      
+            
+            <div class=wrapper>
+                <div class=posts>
+                    <div class=post>
+                        
+                        <h2>title </h2>
+                         {{ $post->title }}
+                        <a>sentence</a>
+                        <div class=post_date>
+                            <p>{{ $post->body }}</p> 
+                           
+                            
+                            <div class=post></div>
+                               
+                        </div>
+                        <a href="/">戻る</a>  
+                            
+                    </div>
+               
+                </div>
+                
+                
+                
+                
+            </div>
+            
+       
+        
+        
+        
+    </body>
